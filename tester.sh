3c2338f330b9db16342641cee1b9ecc7d48de9d0
ffrau <ffrau@student.42roma.it>
2022-06-16 11:27:57 +0200

    Update v2.0.2
    - Added an header and footer for each test
    - Removed stderr messages
    - Removed makefile output
    - Added personal colors for each output test
    	- Green for 100% of tests passed successfully
    	- Yellow for 75% of tests passed successfully
    	- Red for less than 75% of tests passed
    	- For 3 and 5 numbers the color will be red also for 1 test not
    	passed

--- tester.sh
+++ tester.sh
@@ -6,19 +6,61 @@
 #    By: ffrau <ffrau@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/06/11 19:52:02 by ffrau             #+#    #+#              #
-#    Updated: 2022/06/14 13:28:21 by ffrau            ###   ########.fr        #
+#    Updated: 2022/06/16 11:27:51 by ffrau            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-RED="\033[31m"
 IRED="\033[41m"
+BLACK="\033[30m"
+RED="\033[31m"
 LGREEN="\033[32m"
+YELLOW="\033[33m"
 NC="\033[0m"
 CURR_PATH=$(pwd)
 PUSH_SWAP="${CURR_PATH}/../push_swap"
 CHECKER="${CURR_PATH}/../checker"
 CHECKER_MAC="${CURR_PATH}/../checker_Mac"
 ERROR_FILE="errors.txt"
+OUTPUT_FILE="output.txt"
+
+print_message()
+{
+	# echo "$1 || $2 || $3 || $4 || $5 || $6 || $7"
+	# echo "Num || Counter || totalTests || totalAvarage || nMin || nMax || maxMoves"
+	printf "| ${LGREEN}$1${NC}	 | Numbers: You made "
+	if [ $1 -le 5 ] && [ ! $2 == $3 ]; then
+		printf "${RED}${COUNTER}${LGREEN}/$3${NC}"
+	elif [ $1 -le 5 ] && [ $2 == $3 ]; then
+		printf "${LGREEN}${COUNTER}/$3${NC}"
+	fi
+	if [ $1 -gt 5 ] && [ $2 == $3 ]; then
+		printf "${LGREEN}${COUNTER}/$3${NC}"
+	elif [ $1 -gt 5 ] && [ $2 -gt $((($3 / 4) * 3)) ]; then
+		printf "${YELLOW}${COUNTER}${LGREEN}/$3${NC}"
+	elif [ $1 -gt 5 ] && [ $2 -le $((($3 / 4) * 3)) ]; then
+		printf "${RED}${COUNTER}${LGREEN}/$3${NC}"
+	fi
+	if [ $1 == 3 ]; then
+		printf "     Average of "
+	else
+		printf " Average of "
+	fi
+	if [ $(($4 / $3)) -gt $7 ]; then
+		printf "${RED}$(($4 / $3))${NC}	|"
+	else
+		printf "${LGREEN}$(($4 / $3))${NC}	|"
+	fi
+	if [ $5 -le $7 ]; then
+		printf "${LGREEN} $5${NC}	 |"
+	else
+		printf "${RED} $5${NC}	 |"
+	fi
+	if [ $6 -le $7 ]; then
+		printf "${LGREEN} $6${NC}	   |\n"
+	else
+		printf "${RED} $6${NC}	   |\n"
+	fi
+}
 
 three_number(){
 	COUNTER=0
@@ -41,16 +83,16 @@ three_number(){
 						MAX=${RESULT}
 					fi
 					AVERAGE=$((AVERAGE + RESULT))
-					if [ $RESULT -le  3 ]; then
+					if [ $RESULT -le 3 ]; then
 						COUNTER=$((COUNTER +1))
 					else
-						printf "${RED}Error with ${NC}'${PUSH_SWAP} ${i} ${j} ${k}'"
+						printf "${RED}Error with ${NC}'${PUSH_SWAP} ${i} ${j} ${k}'\n" >> ${ERROR_FILE}
 					fi
 				fi
 			done
 		done
 	done
-	printf "${LGREEN}3${NC}	Numbers: You made ${LGREEN}${COUNTER}/6${NC}.		Average of ${LGREEN}$((AVERAGE / 6))	${NC}${LGREEN}[${MIN}]${RED}[${MAX}]${NC}\n"
+	print_message 3 ${COUNTER} 6 ${AVERAGE} ${MIN} ${MAX} 3
 }
 
 five_number(){
@@ -81,7 +123,7 @@ five_number(){
 							if [ $RESULT -le  12 ]; then
 								COUNTER=$((COUNTER +1))
 							else
-								printf "${RED}Error with ${NC}'${PUSH_SWAP} ${i} ${j} ${k} ${x} ${y}'"
+								printf "${RED}Error with ${NC}'${PUSH_SWAP} ${i} ${j} ${k} ${x} ${y}'\n" >> ${ERROR_FILE}
 							fi
 						fi
 					done
@@ -89,7 +131,7 @@ five_number(){
 			done
 		done
 	done
-	printf "${LGREEN}5${NC}	Numbers: You made ${LGREEN}${COUNTER}/120${NC}.	Average of ${LGREEN}$((AVERAGE / 120))	${NC}${LGREEN}[${MIN}]${RED}[${MAX}]${NC}\n"
+	print_message 5 ${COUNTER} 120 ${AVERAGE} ${MIN} ${MAX} 12
 }
 
 one_hundred(){
@@ -119,15 +161,11 @@ one_hundred(){
 		if [ ${RESULT} -le ${MOVES} ]; then
 			COUNTER=$((COUNTER +1))
 		else
-			printf "${RED}Error${NC}\n"
 			echo "'./push_swap ${SEQUENCE}'" >> ${ERROR_FILE}
 			printf "Moves: ${RED}${RESULT}${NC}\n" >> ${ERROR_FILE}
 		fi
 	done
-	printf "${LGREEN}100${NC}	Numbers: You made ${LGREEN}${COUNTER}/${TESTS}${NC}.	Average of ${LGREEN}$((AVERAGE / ${TESTS}))	${NC}${LGREEN}[${MIN}]${RED}[${MAX}]${NC}\n"
-	if [ ! ${COUNTER} == ${TESTS} ]; then
-		printf "You can read the errors with ./tester showerrors\n"
-	fi
+	print_message 100 ${COUNTER} ${TESTS} ${AVERAGE} ${MIN} ${MAX} ${MOVES}
 }
 
 five_hundred(){
@@ -157,15 +195,11 @@ five_hundred(){
 		if [ ${RESULT} -le ${MOVES} ]; then
 			COUNTER=$((COUNTER +1))
 		else
-			printf "${RED}Error${NC}\n"
 			echo "'./push_swap ${SEQUENCE}'" >> ${ERROR_FILE}
 			printf "Moves: ${RED}${RESULT}${NC}\n" >> ${ERROR_FILE}
 		fi
 	done
-	printf "${LGREEN}500${NC}	Numbers: You made ${LGREEN}${COUNTER}/${TESTS}${NC}.	Average of ${LGREEN}$((AVERAGE / ${TESTS}))	${NC}${LGREEN}[${MIN}]${RED}[${MAX}]${NC}\n"
-	if [ ! ${COUNTER} == ${TESTS} ]; then
-		printf "You can read the errors with ./tester showerrors\n"
-	fi
+	print_message 500 ${COUNTER} ${TESTS} ${AVERAGE} ${MIN} ${MAX} ${MOVES}
 }
 
 showerrors(){
@@ -191,42 +225,36 @@ stresstests()
 		OUTPUT_CHECKER=$(${PUSH_SWAP} ${ARG} | ${CHECKER} ${ARG})
 	fi
 	OUTPUT_CHECKER_MAC=$(${PUSH_SWAP} ${ARG} | ${CHECKER_MAC} ${ARG})
-	if [ ${CHECKER_USE} == 0 ] && [ ${CHECKER_MAC} == "KO" ]; then
+	if [ ${CHECKER_USE} == 0 ] && [ ${OUTPUT_CHECKER_MAC} == "KO" ]; then
 		SHOW_ERROR=1
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	elif [ ${CHECKER_USE} == 1 ] && [ ! ${OUTPUT_CHECKER} == ${OUTPUT_CHECKER_MAC} ]; then
 		SHOW_ERROR=1
-	fi
-	if [ ${SHOW_ERROR} == 1 ]; then
-		printf "${RED}Error. The output isn't valid${NC}\n"
-		printf "./push_swap ${ARG}\n"
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	fi
 	ARG="1 5 2 4 3"
 	if [ ${CHECKER_USE} == 1 ]; then
 		OUTPUT_CHECKER=$(${PUSH_SWAP} ${ARG} | ${CHECKER} ${ARG})
 	fi
 	OUTPUT_CHECKER_MAC=$(${PUSH_SWAP} ${ARG} | ${CHECKER_MAC} ${ARG})
-	if [ ${CHECKER_USE} == 0 ] && [ ${CHECKER_MAC} == "KO" ]; then
+	if [ ${CHECKER_USE} == 0 ] && [ ${OUTPUT_CHECKER_MAC} == "KO" ]; then
 		SHOW_ERROR=1
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	elif [ ${CHECKER_USE} == 1 ] && [ ! ${OUTPUT_CHECKER} == ${OUTPUT_CHECKER_MAC} ]; then
 		SHOW_ERROR=1
-	fi
-	if [ ${SHOW_ERROR} == 1 ]; then
-		printf "${RED}Error. The output isn't valid${NC}\n"
-		printf "./push_swap ${ARG}\n"
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	fi
 	ARG="-1 +5 -2 4 3"
 	if [ ${CHECKER_USE} == 1 ]; then
 		OUTPUT_CHECKER=$(${PUSH_SWAP} ${ARG} | ${CHECKER} ${ARG})
 	fi
 	OUTPUT_CHECKER_MAC=$(${PUSH_SWAP} ${ARG} | ${CHECKER_MAC} ${ARG})
-	if [ ${CHECKER_USE} == 0 ] && [ ${CHECKER_MAC} == "KO" ]; then
+	if [ ${CHECKER_USE} == 0 ] && [ ${OUTPUT_CHECKER_MAC} == "KO" ]; then
 		SHOW_ERROR=1
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	elif [ ${CHECKER_USE} == 1 ] && [ ! ${OUTPUT_CHECKER} == ${OUTPUT_CHECKER_MAC} ]; then
 		SHOW_ERROR=1
-	fi
-	if [ ${SHOW_ERROR} == 1 ]; then
-		printf "${RED}Error. The output isn't valid${NC}\n"
-		printf "./push_swap ${ARG}\n"
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	fi
 	ARG="$(ruby -e "puts (-50..48).to_a.shuffle.join(' ')")"
 	if [ ${CHECKER_USE} == 1 ]; then
@@ -235,19 +263,25 @@ stresstests()
 	OUTPUT_CHECKER_MAC=$(${PUSH_SWAP} ${ARG} | ${CHECKER_MAC} ${ARG})
 	if [ ${CHECKER_USE} == 0 ] && [ ${CHECKER_MAC} == "KO" ]; then
 		SHOW_ERROR=1
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	elif [ ${CHECKER_USE} == 1 ] && [ ! ${OUTPUT_CHECKER} == ${OUTPUT_CHECKER_MAC} ]; then
 		SHOW_ERROR=1
+		printf "./push_swap ${ARG}\n" >> ${ERROR_FILE}
 	fi
 	if [ ${SHOW_ERROR} == 1 ]; then
-		printf "${RED}Error. The output isn't valid${NC}\n"
-		printf "./push_swap ${ARG}\n"
+		printf "${IRED}ERROR${NC}${RED}. There are some error with the checkers${NC}\n"
+		read -rp "Do you want to continue with the tests? [y/n] " ANSWER
+		if [ ${ANSWER} == 'n' ] || [ ${ANSWER} == 'N' ]; then
+			exit 0
+		fi
+	else
+		printf "${LGREEN}Checker	test passed successfully ✅${NC}\n"
 	fi
 }
 
 output_validator()
 {
 	SHOW_ERROR=0
-	printf "${RED}Default tests${NC}\n"
 	OUTPUT=$(${PUSH_SWAP} 42 | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
@@ -270,40 +304,39 @@ output_validator()
 			exit 0
 		fi
 	else
-		printf "${LGREEN}Test passed successfully${NC}\n"
+		printf "${LGREEN}Output	test passed successfully ✅${NC}\n"
 	fi
 }
 
 stderr_validator()
 {
 	SHOW_ERROR=0
-	printf "${RED}fd tests. Five error messages will be printed.${NC}\n"
-	OUTPUT=$(${PUSH_SWAP} a | wc -l)
+	OUTPUT=$(${PUSH_SWAP} a 2> /dev/null | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
 	fi
 
-	OUTPUT=$(${PUSH_SWAP} 1 1 | wc -l)
+	OUTPUT=$(${PUSH_SWAP} 1 1 2> /dev/null | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
 	fi
 
-	OUTPUT=$(${PUSH_SWAP} 1 -2147483649 | wc -l)
+	OUTPUT=$(${PUSH_SWAP} 1 -2147483649 2> /dev/null | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
 	fi
 
-	OUTPUT=$(${PUSH_SWAP} 1 +2147483648 | wc -l)
+	OUTPUT=$(${PUSH_SWAP} 1 +2147483648 2> /dev/null | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
 	fi
 
-	OUTPUT=$(${PUSH_SWAP} +-"1" 3| wc -l)
+	OUTPUT=$(${PUSH_SWAP} +-"1" 3 2> /dev/null | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
 	fi
 
-	OUTPUT=$(${PUSH_SWAP} +-"1" 3| wc -l)
+	OUTPUT=$(${PUSH_SWAP} +-"1" 3 2> /dev/null | wc -l)
 	if [ ! ${OUTPUT} == 0 ]; then
 		SHOW_ERROR=1
 	fi
@@ -315,7 +348,7 @@ stderr_validator()
 			exit 0
 		fi
 	else
-		printf "${LGREEN}Test passed successfully${NC}\n"
+		printf "${LGREEN}FD	test passed successfully ✅${NC}\n"
 	fi
 }
 
@@ -327,14 +360,14 @@ makefile_checker()
 		printf "${RED}Error, Makefile not found. I can't compile the program\n${NC}"
 		exit 0
 	fi
-	make re -C ${CURR_PATH}/../
+	make re -C ${CURR_PATH}/../ >> ${OUTPUT_FILE}
 	if [ ! -f ${CHECKER_MAC} ]; then
 		CHECKERP=0
-		curl -o checker_Mac https://projects.intra.42.fr/uploads/document/document/8245/checker_Mac
+		curl -o checker_Mac https://projects.intra.42.fr/uploads/document/document/8245/checker_Mac 2> /dev/null
 		chmod +x ./checker_Mac
 		mv checker_Mac ../
 	fi
-	make clean -C ${CURR_PATH}/../
+	make clean -C ${CURR_PATH}/../ >> ${OUTPUT_FILE}
 	if [ ! -f ${PUSH_SWAP} ]; then
 		printf "${IRED}ERROR${NC}${RED}. push_swap not found. The 'clean' method also deletes the executable${NC}\n"
 		read -rp "Do you want to continue with the tests? [y/n] " ANSWER
@@ -354,8 +387,9 @@ makefile_checker()
 		fi
 	fi
 	if [ ${RECOMPILE} == 1 ]; then
-		make re -C ${CURR_PATH}/../
+		make re -C ${CURR_PATH}/../ >> ${OUTPUT_FILE}
 	fi
+	rm -rf ${OUTPUT_FILE}
 }
 
 press_any_key(){
@@ -365,8 +399,9 @@ press_any_key(){
 
 end_test(){
 	press_any_key
-	make fclean -C ${CURR_PATH}/../
+	make fclean -C ${CURR_PATH}/../ >> ${OUTPUT_FILE}
 	rm -rf ${CHECKER_MAC}
+	rm -rf ${OUTPUT_FILE}
 	exit
 }
 
@@ -396,6 +431,31 @@ norme()
 	fi
 }
 
+header()
+{
+	printf "+--------+----------------------------------------------+--------+---------+\n"
+	printf "| ${LGREEN}Numbers${NC}|						|${LGREEN} Min    ${NC}| ${LGREEN}Max     ${NC}|\n"
+	printf "+--------+----------------------------------------------+--------+---------+\n"
+}
+
+footer()
+{
+	printf "+--------+----------------------------------------------+--------+---------+\n"
+	if [ -f ${ERROR_FILE} ]; then
+		printf "You can read the errors with ./tester showerrors\n"
+	else
+		printf "${LGREEN}No errors. Good job!${NC}\n"
+	fi
+}
+
+single_compile()
+{
+	if [ ! -f ${CURR_PATH}/../Makefile ]; then
+		printf "${RED}Error, Makefile not found. I can't compile the program\n${NC}"
+	fi
+	make re -C ${CURR_PATH}/../ >> ${OUTPUT_FILE}
+	rm -rf ${OUTPUT_FILE}
+}
 
 valid_params()
 {
@@ -408,10 +468,12 @@ valid_params()
 		stderr_validator
 		output_validator
 		stresstests
+		header
 		three_number
 		five_number
 		one_hundred
 		five_hundred
+		footer
 	else
 		case $1 in
 			"makefile")
@@ -419,29 +481,36 @@ valid_params()
 				end_test
 				;;
 			"errors")
-				make re -C ${CURR_PATH}/../
 				stderr_validator
 				output_validator
 				end_test
 				;;
 			'3')
-				make re -C ${CURR_PATH}/../
+				single_compile
+				header
 				three_number
+				footer
 				end_test
 				;;
 			'5')
-				make re -C ${CURR_PATH}/../
+				single_compile
+				header
 				five_number
+				footer
 				end_test
 				;;
 			"100")
-				make re -C ${CURR_PATH}/../
+				single_compile
+				header
 				one_hundred $2 $3
+				footer
 				end_test
 				;;
 			"500")
-				make re -C ${CURR_PATH}/../
+				single_compile
+				header
 				five_hundred $2 $3
+				footer
 				end_test
 				;;
 			"list")
